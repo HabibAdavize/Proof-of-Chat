12eee919af498395b7947bc287617d3e47f54e6f
AlexKeshi <kesinroalex3@gmail.com>
2025-03-14 04:24:20 +0100

    Ookay I am done

--- src/components/MintProofButton.jsx
+++ src/components/MintProofButton.jsx
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import abi from "../abi.json";
 
-const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+const contractAddress = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8";
 
 function MintProofButton({ user }) {
   const mintProof = async () => {
